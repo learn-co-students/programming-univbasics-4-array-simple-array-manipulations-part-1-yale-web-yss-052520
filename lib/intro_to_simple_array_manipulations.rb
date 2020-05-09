8e1296f8b72bf3cbe55c4b9c0fb26b0ff40b67dd
Alexis Teh <wenhui.teh23@gmail.com>
2020-05-09 23:17:27 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,24 @@
+
+def using_push (array, string)
+  array.push(string)
+end 
+
+def using_unshift (array, string)
+  array.unshift(string)
+end 
+
+def using_pop (a)
+  a.pop 
+end 
+
+def pop_with_args (a)
+  a.pop(2)
+end 
+
+def using_shift (a) 
+  a.shift 
+end 
+
+def shift_with_args (a)
+  a. shift(2)
+end 
